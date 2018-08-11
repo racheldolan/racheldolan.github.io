ae6ac1a63e1d5096662f70fd5ee8bdcb82a98a51
Rachel Dolan <racheldolan1@gmail.com>
2018-08-11 14:50:01 +0100

    added side navbar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('js loaded');
+// console.log('js loaded');
+//
+// $(window).ready(function() {
+//
+//   const menu = $('.menu li:nth-child(2), .menu li:nth-child(3), .menu li:nth-child(4), .menu li:nth-child(5)');
+//
+//   $('#navbar-btn span').click(function(){
+//
+//     menu.toggle(550);
+//
+//     if ($('#open').is(':visible')){
+//       $('#open').hide();
+//       $('#close').fadeIn(200);
+//     } else {
+//       $('#close').hide();
+//       $('#open').fadeIn(200);
+//     }
+//
+//   });
+//
+// });
